735070045f1d6e4345de2b47bbf0d0e21c46fac4
RachdianZulkarnain <rachdianmuhammadadha@gmail.com>
2025-08-21 10:48:51 +0700

    chore: init setup api

--- src/utils/api-error.ts
+++ src/utils/api-error.ts
@@ -1,7 +1,10 @@
 export class ApiError extends Error {
-  status: number;
-  constructor(massage: string, status: number) {
-    super(massage);
-    this.status = status;
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    Object.setPrototypeOf(this, new.target.prototype);
+    Error.captureStackTrace(this);
   }
 }
